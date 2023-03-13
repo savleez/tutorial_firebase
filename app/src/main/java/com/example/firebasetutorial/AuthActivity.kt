3ebd4e02c5bdf42901cce5c4777aebdffb564874
Sergio Velez <svleez@gmail.com>
2023-03-12 22:46:42 -0500

    Autenticacion con google

--- app/src/main/java/com/example/firebasetutorial/AuthActivity.kt
+++ app/src/main/java/com/example/firebasetutorial/AuthActivity.kt
@@ -1,17 +1,35 @@
 package com.example.firebasetutorial
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
+    private lateinit var signUpButton: Button
+    private lateinit var loginButton: Button
+    private lateinit var googleButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var authLayout: LinearLayout
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
@@ -31,15 +49,39 @@ class AuthActivity : AppCompatActivity() {
         // Setup
         setup()
 
+        // Sesion
+        session()
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        authLayout.visibility = View.VISIBLE
+    }
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
+
     }
 
     private fun setup() {
         title = "Autenticación"
 
-        val signUpButton = findViewById<Button>(R.id.signUpButton)
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        signUpButton = findViewById<Button>(R.id.signUpButton)
+        loginButton = findViewById<Button>(R.id.loginButton)
+        googleButton = findViewById<Button>(R.id.googleButton)
+        emailEditText = findViewById<EditText>(R.id.emailEditText)
+        passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        authLayout = findViewById<LinearLayout>(R.id.authLayout)
+
 
         signUpButton.setOnClickListener {
             if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
@@ -72,6 +114,20 @@ class AuthActivity : AppCompatActivity() {
                     }
             }
         }
+
+        googleButton.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
     }
 
     private fun showAlert() {
@@ -90,5 +146,31 @@ class AuthActivity : AppCompatActivity() {
             putExtra("provider", provider.name)
         }
         startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert()
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/firebasetutorial/HomeActivity.kt
+++ app/src/main/java/com/example/firebasetutorial/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firebasetutorial
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -7,7 +8,8 @@ import com.google.firebase.auth.FirebaseAuth
 
 
 enum class ProviderType {
-    BASIC
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -21,6 +23,14 @@ class HomeActivity : AppCompatActivity() {
         val provider = bundle?.getString("provider")
 
         setup(email ?: "", provider ?: "")
+
+
+        // Persistencia de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
     }
 
     private fun setup(email: String, provider: String) {
@@ -34,8 +44,16 @@ class HomeActivity : AppCompatActivity() {
         providerTextView.text = provider
 
         logOutButton.setOnClickListener {
+            // Logout de Firebase
             FirebaseAuth.getInstance()
                 .signOut()
+
+            // Borrado de datos persistentes
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            // Regreso a pantalla anterior
             onBackPressedDispatcher.onBackPressed()
         }
     }
